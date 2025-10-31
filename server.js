4f3715b4569db44949a267473cb1b24194a29adf
ArturRushMFM <artur@mfmsoft.com>
2025-10-31 16:25:26 +0400

    Add corse

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const config = require("./config/config");
+const cors = require("corse");
 
 const startServer = async (app) => {
   try {
+    app.use(cors());
+
     app.listen(config.PORT, () => {
       console.log(`Server is up on port: ${config.PORT}`);
     });
