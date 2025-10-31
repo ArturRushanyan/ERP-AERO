57a4c7add424605f992c7c4a3e780bca109f7175
ArturRushMFM <artur@mfmsoft.com>
2025-10-31 16:21:37 +0400

    Added env file exmple and start reading env config

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+const config = require("./config/config");
+
 const startServer = async (app) => {
   try {
-    app.listen(3000, () => {
-      console.log(`Server is up on port: ${3000}`);
+    app.listen(config.PORT, () => {
+      console.log(`Server is up on port: ${config.PORT}`);
     });
   } catch (error) {
     console.log("Server is not running:", error);
