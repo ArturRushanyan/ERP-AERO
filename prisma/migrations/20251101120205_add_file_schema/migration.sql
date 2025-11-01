5afce9d17dbfad896f0470f26f031b442c39f1eb
ArturRushMFM <artur@mfmsoft.com>
2025-11-01 16:04:13 +0400

    Change ORM to Prisma and add File schema table

---
+++ prisma/migrations/20251101120205_add_file_schema/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `FileRecord` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `ext` VARCHAR(255) NOT NULL,
+    `mime` VARCHAR(255) NOT NULL,
+    `size` INTEGER NOT NULL,
+    `upload_date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
