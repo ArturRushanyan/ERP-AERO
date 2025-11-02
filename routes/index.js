4425c3d16b9abbfb7dc1de8e6b7f12de7c1f5e81
ArturRushMFM <artur@mfmsoft.com>
2025-11-02 14:55:12 +0400

    Added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const fileRoutes = require("./fileRoutes");
+const userRoutes = require("./userRoutes");
 
 // Use file routes
 router.use("/file", fileRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
